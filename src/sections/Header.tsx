c4d3994f4a9ed76f2e0943bfc700fa7184404c9d
sammythedeveloper <justsammy484@gmail.com>
2025-02-03 17:27:49 -0500

    second

---
+++ src/sections/Header.tsx
@@ -0,0 +1,14 @@
+export const HeaderSection = () => {
+    return <div>
+        <div className=" container" >
+            <div className="flex justify-between">
+                <div>
+                    <img src="/assets/images/logo.svg" alt="" />
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>;
+};
