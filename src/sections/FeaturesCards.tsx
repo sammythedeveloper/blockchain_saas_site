3ff2e295b0cd538ee438ace28d7cfc333a09ea95
sammythedeveloper <justsammy484@gmail.com>
2025-02-05 15:43:21 -0500

    feature

---
+++ src/sections/FeaturesCards.tsx
@@ -0,0 +1,13 @@
+export const FeaturesCardsSections = () => {
+    return <section>
+        <div className=" container " >
+            <h2>Discover the future of blockchain with BlockForge.</h2>
+            <div>
+                <div>
+                    <h3>Revolutionary Blockchain API</h3>
+                    <p>Effortlessly integrate and manage blockchain data with cutting age API ,designed for seamless connectivity</p>
+                </div>
+            </div>
+        </div>
+    </section>;
+};
