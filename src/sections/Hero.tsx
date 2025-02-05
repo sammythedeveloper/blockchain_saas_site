bded752051f655d58116475a608f093a85053fc1
sammythedeveloper <justsammy484@gmail.com>
2025-02-04 20:22:07 -0500

    hexagon

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,5 @@
 import { CutCornerButton } from "../components/CutCornerButton";
+import { Hexagon } from "../components/Hexagon";
 export const Herosection = () => {
   return (
     <section className=" py-24 md:py-52 overflow-x-clip ">
@@ -9,7 +10,7 @@ export const Herosection = () => {
         <h1 className=" font-heading font-black text-5xl md:text-6xl lg:text-7xl text-center mt-4 max-w-3xl mx-auto ">
           The Future of Blockchain is Here
         </h1>
-        <p className=" text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl">
+        <p className=" text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto ">
           Blockforge is pioneering smart contract integrity with cutting-edge
           data solutions.
         </p>
@@ -18,6 +19,9 @@ export const Herosection = () => {
         </div>
         <div className=" flex justify-center mt-24">
           <div className=" inline-flex relative z-0 ">
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+              <Hexagon className="size-[1100px]" />
+            </div>
             <img
               src="/assets/images/icosahedron.png"
               alt=""
