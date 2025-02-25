fe2cbff7621abe03fa57a40c6a32c924e29c0f70
sammythedeveloper <justsammy484@gmail.com>
2025-02-24 22:50:46 -0500

    my own style

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,13 +4,17 @@ export const HeaderSection = () => {
     <header className=" sticky top-0 z-40 bg-zinc-900/50 backdrop-blur-lg ">
       <div className=" container">
         <div className="flex justify-between items-center h-24 md:h-28">
-          <div>
-            <img src="/assets/images/logo.svg" alt="" />
+          <div className=" font-heading text-4xl font-bold  ">
+            <h2>Flowture</h2>
           </div>
           <div className=" flex gap-4 items-center">
             <CutCornerButton className=" hidden md:inline-flex  ">
               {" "}
-              Get Started
+              Sign Up
+            </CutCornerButton>
+            <CutCornerButton className=" hidden md:inline-flex  ">
+              {" "}
+              Sign in
             </CutCornerButton>
             <div className="size-10 relative">
               <div className=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,14 +6,13 @@ export const Herosection = () => {
     <section className=" py-24 md:py-52 overflow-x-clip ">
       <div className=" container">
         <p className=" uppercase font-extrabold text-center text-zinc-500 tracking-wider">
-          Introducing Blockforge
+          Introducing Flowture
         </p>
         <h1 className=" font-heading font-black text-5xl md:text-6xl lg:text-7xl text-center mt-4 max-w-3xl mx-auto ">
-          The Future of Blockchain is Here
+          Where Time Meets Efficiency
         </h1>
         <p className=" text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto ">
-          Blockforge is pioneering smart contract integrity with cutting-edge
-          data solutions.
+          Flowture is your smart planner that helps you organize,prioritize and achieve moree with less effort day by day.
         </p>
         <div className=" flex justify-center mt-10">
           <CutCornerButton>Get Started</CutCornerButton>
